469a4e89af01f0f945e46c330271ff5a10ae4999
MaJeD <majed@prine.ch>
2018-05-12 13:39:20 +0200

    initial commit with week 8 example

---
+++ week 8/js/app.js
@@ -0,0 +1,16 @@
+function sum() {
+    var a = document.getElementById("A").value;
+    var b = document.getElementById("B").value;
+
+    var c = Number(a) + Number(b) ;
+
+    document.getElementById("C").value = c;
+
+    if (c == 0 ) {
+        document.getElementById("C").style.backgroundColor = "red";        
+    } else if (c%2 == 0) {
+        document.getElementById("C").style.backgroundColor = "lightgreen";
+    } else if (c%2 == 1) {
+        document.getElementById("C").style.backgroundColor = "lightblue";        
+    }
+}
